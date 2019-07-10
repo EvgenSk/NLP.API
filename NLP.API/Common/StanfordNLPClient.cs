474d5937947e99da176fd64d0c67a40a0b1c278e
EvgenSk <evgensk@gmail.com>
2019-07-10 09:25:59 +0700

    - add typed ProcessTextAsync - get rid of separate file for Annotator

--- NLP.API/Common/StanfordNLPClient.cs
+++ NLP.API/Common/StanfordNLPClient.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using NLP.API.Common.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace NLP.API.Common
 {
@@ -18,18 +20,35 @@ namespace NLP.API.Common
 		}
 
 		string EndPoint => Options.Port == 0 ? $"{Options.Host}" : $"{Options.Host}:{Options.Port}";
+		static List<Annotator> AllAnnotators { get; } = Enum.GetValues(typeof(Annotator)).Cast<Annotator>().ToList();
 
 		/// <summary>
 		/// Processes given text using selected annotators
 		/// </summary>
 		/// <param name="text">Text to be processed</param>
 		/// <param name="annotator">Annotators - flags - should be connected via |</param>
+		/// <returns>Text, annotated with given annotators</returns>
+		public async Task<AnnotatedText> ProcessTextAsync(string text, Annotator annotator)
+		{
+			string rawText = await ProcessTextRawResultAsync(text, annotator, OutputFormat.JSON);
+			return 
+				string.IsNullOrEmpty(rawText)
+				? null
+				: JsonConvert.DeserializeObject<AnnotatedText>(rawText);
+		}
+
+		/// <summary>
+		/// Processes given text using selected annotators
+		/// </summary>
+		/// <param name="text">Text to be processed</param>
+		/// <param name="annotator">Annotators - flags - should be connected via |</param>
+		/// <param name="outputFormat">Output Format - one of [JSON, XML, Text]</param>
 		/// <returns>JSON output from Stanford NLP service</returns>
-		public async Task<string> ProcessTextAsync(string text, Annotator.Type annotator)
+		public async Task<string> ProcessTextRawResultAsync(string text, Annotator annotator, OutputFormat outputFormat = OutputFormat.JSON)
 		{
 			using (var httpClient = new HttpClient())
 			{
-				string queryString = $"{EndPoint}/?{GetPropertiesString(annotator)}";
+				string queryString = $"{EndPoint}/?{GetPropertiesString(annotator, outputFormat)}";
 				var response = await httpClient.PostAsync(queryString, new StringContent(text));
 				return 
 					response.StatusCode == HttpStatusCode.OK 
@@ -37,19 +56,34 @@ namespace NLP.API.Common
 					: string.Empty;
 			}
 		}
-		string GetPropertiesString(IEnumerable<Annotator.Type> annotators)
+
+		string GetPropertiesString(Annotator annotator, OutputFormat outputFormat)
+		{
+			var annotators = AllAnnotators.Select(a => annotator & a).Where(a => a != Annotator.None);
+			return GetPropertiesString(annotators, outputFormat);
+		}
+
+		string GetPropertiesString(IEnumerable<Annotator> annotators, OutputFormat outputFormat)
 		{
 			var strings = annotators.Select(a => a.ToString().ToLower());
 			var stringAnnotators = string.Join(",", strings);
 
-			string parametersJson = $"\"annotators\":\"{stringAnnotators}\",\"outputFormat\":\"json\"";
+			string parametersJson = $"\"annotators\":\"{stringAnnotators}\",\"outputFormat\":\"{outputFormat.ToString().ToLowerInvariant()}\"";
 			return "properties={" + parametersJson + "}";
 		}
+	}
 
-		string GetPropertiesString(Annotator.Type annotator)
-		{
-			var annotators = Annotator.AllAnnotators.Select(a => annotator & a).Where(a => a != Annotator.Type.None);
-			return GetPropertiesString(annotators);
-		}
+	public enum OutputFormat { JSON, XML, Text }
+
+	[Flags]
+	public enum Annotator
+	{
+		None = 0,
+		Tokenize = 1,
+		Ssplit = 2,
+		POS = 4,
+		Lemma = 8,
+		NER = 16,
+		RegexNER = 32
 	}
 }
